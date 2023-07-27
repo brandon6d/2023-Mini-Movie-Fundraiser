bccd7876a7f6163e9b709038be8ef085e8c2b42c
brandon6d <136387238+brandon6d@users.noreply.github.com>
2023-07-27 13:16:26 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,12 @@
+# functions go here
+
+
+# main routine goes here
+want_instructions = input("do you want instructions?")
+
+if want_instructions == "yes":
+    print("Instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes / no")
